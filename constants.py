06b93f90bc955f7b903e64d0f589668ca91c9666
tommy-dart <tommy@cs.dartmouth.edu>
2021-05-31 19:59:31 -0400

    Added more units for pv=nrt calculation in constants.py

--- constants.py
+++ constants.py
@@ -12,13 +12,25 @@ Measurement = namedtuple('Measurement', ['value', 'units'])
 
 # air pressure
 NMOLS = 'n_moles'
-TEMPERATURE = 'temperature'
-VOLUME = 'volume'
+N_ATOMS = 'n_atoms'
+GRAMS = 'grams'
+KG = 'kg'
+POUNDS = 'pounds'
 
 PRESSURE = 'pressure'
 ATM = 'atm'
 KPA = 'kpa'
 MMHG = 'mmhg'
+
+TEMPERATURE = 'temperature'
+KELVIN = 'kelvin'
+CELSIUS = 'celsius'
+FARENHEIT = 'farenheit'
+
+VOLUME = 'volume'
+LITERS = 'liters'
+MILILITERS = 'ml'
+GALLONS = 'gallons'
 
 # stoichiometry
 REAGENTS = 'reagents'

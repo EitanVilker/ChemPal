c726a6f89174d0e14ebe50c682e3c96ce0698d8b
tommy-dart <tommy@cs.dartmouth.edu>
2021-05-31 19:51:24 -0400

    Added method headers for conversion functions

--- constants.py
+++ constants.py
@@ -15,12 +15,16 @@ NMOLS = 'n_moles'
 PRESSURE = 'pressure'
 TEMPERATURE = 'temperature'
 VOLUME = 'volume'
+ATM = 'atm'
+KPA = 'kpa'
+MMHG = 'mmhg'
 
 # stoichiometry
 REAGENTS = 'reagents'
 PRODUCTS = 'products'
 
 ## query types
+AIR_PRESSURE - 'air_pressure'
 ATOMIC_WEIGHT = 'atomic_weight'
 ELEM_USES = 'element_uses'
 MATH = 'arithmetic'
